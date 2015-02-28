1e00be89f24a9226649b17e8ebea686fc0bde5c4
Yeo Jing Kang <yeojingkang@hotmail.com>
2015-02-28 20:11:02 +0800

    Added music managers to all scenes that can change the music that plays throughout the game.(No need to touch GameMusic obj)

---
+++ SP4/Assets/Scripts/0A. All Levels/MusicManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManagerScript : MonoBehaviour {
+	public AudioClip NewMusic;
+	public bool StopOnStart;
+
+	AudioSource gameMusic;
+
+	void Awake () {
+		gameMusic = GameObject.Find("Game Music").audio;	//Finds the game object called Game Music 
+
+		if(NewMusic != gameMusic.clip && NewMusic != null){	//If audio is the same, don't change current audio
+			gameMusic.clip = NewMusic;	//Replaces the old audio with the new one set in the inspector.
+			gameMusic.Play();			//Plays the audio.
+		}
+
+		if(StopOnStart){
+			gameMusic.Stop();
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void changeMusic(AudioClip changeMusic){
+		if(changeMusic != gameMusic.clip && changeMusic != null){	//If audio is the same, don't change current audio
+			gameMusic.clip = changeMusic;	//Replaces the old audio with the new one set in the inspector.
+			gameMusic.Play();			//Plays the audio.
+		}
+	}
+
+	public void Play(){
+		gameMusic.Play();
+	}
+	public void Pause(){
+		gameMusic.Pause();
+	}
+	public void Stop(){
+		gameMusic.Stop();
+	}
+}
