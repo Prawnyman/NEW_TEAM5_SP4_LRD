507fc984b0858c8f7dcffaa3344ac291f6b959a8
SunShuyi <prettycure_16@hotmail.com>
2015-03-03 10:06:26 +0800

    FlappyAeroPlaneSoundEffects

--- SP4/Assets/Scripts/10. FlappyAeroPlane/AeroPlane.cs
+++ SP4/Assets/Scripts/10. FlappyAeroPlane/AeroPlane.cs
@@ -6,22 +6,25 @@ public class AeroPlane : MonoBehaviour
 	public float FlySpeed = 1.0f;
 	public Vector2 jumpForce = new Vector2(0, 300);
 	//Vector3 velocity = Vector3.zero;
-
-
+	
+	
 	public AudioClip winSound;
 	public AudioClip loseSound;
+	public AudioClip takeoff;
+	public AudioClip jump;
 	
 	GameObject Timer;
 	
 	private bool gameEnd = true;
-
+	
 	// Use this for initialization
 	void Start () 
 	{
 		GlobalVariables.lastLevel = Application.loadedLevel;
 		Timer = GameObject.FindGameObjectWithTag("timer");
+		TakeOff ();
 	}
-
+	
 	// Update is called once per frame
 	void Update () 
 	{		
@@ -29,17 +32,19 @@ public class AeroPlane : MonoBehaviour
 		{
 			if (Input.GetKeyUp ("space") || Input.touchCount > 0)
 			{
+				audio.clip = jump;
+				audio.Play();
 				rigidbody2D.velocity = Vector2.zero;
 				rigidbody2D.AddForce (jumpForce);
 			}
-
+			
 			Vector2 screenPosition = Camera.main.WorldToScreenPoint (transform.position);
 			if (screenPosition.y > Screen.height || screenPosition.y < 0)
 			{
 				//GameOver();
 			}	
 		}
-
+		
 		if (Timer.GetComponent<TimerScript>().timeLeft <= 0)
 		{
 			if (gameEnd) {
@@ -48,13 +53,13 @@ public class AeroPlane : MonoBehaviour
 			}
 		}
 	}
-
+	
 	void FixedUpdate()
 	{
 		if (gameEnd)
 			rigidbody2D.AddForce (Vector2.right * FlySpeed);
 	}
-
+	
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (gameEnd)
@@ -63,7 +68,27 @@ public class AeroPlane : MonoBehaviour
 			StartCoroutine(Lose());
 		}
 	}
-
+	
+	private IEnumerator TakeOff()
+	{
+		if (!audio.isPlaying) 
+		{
+			audio.clip = winSound;
+			audio.Play ();
+		}
+		yield return new WaitForSeconds (2.4f);
+	}
+	
+	private IEnumerator Jump()
+	{
+		if (!audio.isPlaying) 
+		{
+			audio.clip = winSound;
+			audio.Play ();
+		}
+		yield return new WaitForSeconds (1.0f);
+	}
+	
 	private IEnumerator Win()
 	{
 		GlobalVariables.levelPassed = true;
@@ -71,7 +96,7 @@ public class AeroPlane : MonoBehaviour
 			audio.clip = winSound;
 			audio.Play();
 		}
-		yield return new WaitForSeconds (winSound.length);
+		yield return new WaitForSeconds (2.4f);
 		Application.LoadLevel ("0B. Level Transition");
 	}
 	
@@ -82,7 +107,7 @@ public class AeroPlane : MonoBehaviour
 			audio.clip = loseSound;
 			audio.Play();
 		}
-		yield return new WaitForSeconds (loseSound.length);
+		yield return new WaitForSeconds (1.1f);
 		Application.LoadLevel ("0B. Level Transition");
 	}
 }
